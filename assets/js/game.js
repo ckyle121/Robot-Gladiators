b4ee277c6e0e763d4541b7da372e6514d0b293df
ckyle121 <carolinekyle21@gmail.com>
2021-12-19 10:13:35 -0600

    add Robot's name

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,12 @@
-window.alert("This is an alert! JavaScript is running!");
+// ask player for Robot Name
+
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+
+// create function "fight"
+
+function fight(){
+    window.alert("The fight has begun!")
+}
+
+//fight();
