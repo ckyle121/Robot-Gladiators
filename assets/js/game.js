89ef07dbad1c3a4755765b2207e133c7240c57fe
ckyle121 <carolinekyle21@gmail.com>
2021-12-22 13:52:45 -0600

    add highscore to local storage

--- assets/js/game.js
+++ assets/js/game.js
@@ -175,25 +175,35 @@ var startGame = function(){
 
 // function to end the entire game
 var endGame = function(){
-    // if player is still alive, player wins! if not, player loses
-    if (playerInfo.health > 0){
-        window.alert("Great job, you survived the game! You now have a score of " + playerInfo.money + ".");
+  window.alert("The game has now ended. Let's see how you did... ")
 
-    }
-    else {
-        window.alert("You've lost your robot in battle.")
-    } 
+  // check localStorage for high score, if it's not there, use 0 
+  var highScore = localStorage.getItem("highscore");
+  if (highScore === null){
+    highScore = 0;
+  }
 
-    // ask player if they want to play again 
-    var playAgainConfirm = window.confirm("Would you like to play again?");
+  //if player has more moeny than the high score, player has new high score
+  if (playerInfo.money > highScore){
+    localStorage.setItem("highscore", playerInfo.money);
+    localStorage.setItem("name", playerInfo.name);
 
-    if (playAgainConfirm){
-        //restart the game 
-        startGame();
-    }
-    else{
-        window.alert("Thank you for playing Robot Gladiators. Come back to battle again soon!")
-    }
+    alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+  }
+  else{
+    alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
+  }
+
+  // ask player if they want to play again 
+  var playAgainConfirm = window.confirm("Would you like to play again?");
+
+  if (playAgainConfirm){
+    startGame();
+  }
+  else {
+    window.alert("Thank you for playing Robot Gladiators! Come back and battle again soon.");
+  }
+
 };
 
 // function to shop for health and money 
