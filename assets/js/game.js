061c7b9ff9138495afe9f73dc0809974b294a740
ckyle121 <carolinekyle21@gmail.com>
2021-12-22 14:32:39 -0600

    bug fixes

--- assets/js/game.js
+++ assets/js/game.js
@@ -22,27 +22,31 @@ var fightOrSkip = function() {
   // ask player if they'd like to fight or skip using fightOrSkip function
   var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
 
+  promptFight = promptFight.toLowerCase();
+
   // Enter the conditional recursive function call here!
   if (promptFight === "" || promptFight === null) {
     window.alert("You need to provide a valid answer! Please try again.");
     return fightOrSkip();
   }
   // if player picks "skip" confirm and then stop the loop
-  if (promptFight === "skip" || promptFight === "SKIP") {
+  if (promptFight === "skip") {
     // confirm player wants to skip
     var confirmSkip = window.confirm("Are you sure you'd like to quit?");
 
     // if yes (true), leave fight
     if (confirmSkip) {
       window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
-      // subtract money from playerMoney for skipping
-      playerInfo.playerMoney = playerInfo.money - 10;
-      shop();
+      // subtract money from playerMoney for skipping, but don't let them go into the negative
+      playerInfo.money = Math.max(0, playerInfo.money - 10);
+
+      // return true if player wants to leave
+      return true;
     }
   }
+  return false;
 }
 
-// create function "fight"
 var fight = function(enemy) {
   // keep track of who goes first
   var isPlayerTurn = true;
